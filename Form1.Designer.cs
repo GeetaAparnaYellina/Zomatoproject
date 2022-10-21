e5476880d26ee5e27f6db6884b3317b06eed241a
YELLINA GEETA APARNA <geethaaparna9999@gmail.com>
2022-10-21 14:32:53 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Zomato
+{
+    partial class frm_front
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Order = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(268, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(764, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Online Food Ordering System";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_Order
+            // 
+            this.btn_Order.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Order.Location = new System.Drawing.Point(575, 213);
+            this.btn_Order.Name = "btn_Order";
+            this.btn_Order.Size = new System.Drawing.Size(206, 73);
+            this.btn_Order.TabIndex = 1;
+            this.btn_Order.Text = "Order Now";
+            this.btn_Order.UseVisualStyleBackColor = true;
+            this.btn_Order.Click += new System.EventHandler(this.btn_Order_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.Location = new System.Drawing.Point(575, 307);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(206, 73);
+            this.btn_exit.TabIndex = 2;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(461, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 3;
+            // 
+            // frm_front
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(1215, 642);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_Order);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_front";
+            this.Text = "Front";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Order;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zomato
+{
+    public partial class frm_front : Form
+    {
+        public frm_front()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Form item = new frm_item();
+            item.Show();
+            this.Hide();
+        }
+
+        private void btn_Order_Click(object sender, EventArgs e)
+        {
+            Form item = new frm_item();
+            item.Show();
+            this.Hide();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            DialogResult iExit;
+            iExit = MessageBox.Show("Confirm once you want to exit",
+                "Online Food Ordering System", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(iExit == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+    }
+}
